bb1e3484006ff66fda20c488fa7f3c6d0b775e37
hdmsl18 <sl146@hdm-stuttgart.de>
2022-04-07 18:27:53 +0200

    examples clustering audio data

---
+++ create_dataset_sascha/k_means_example.py
@@ -0,0 +1,12 @@
+import numpy as np
+import scipy
+import matplotlib.pyplot as plt
+import sklearn
+import librosa
+import mir_eval 
+import IPython.display
+import urllib
+
+plt.rcParams['figure.figsize'] = (14, 4)
+
+
