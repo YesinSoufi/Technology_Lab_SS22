adfa2334ee714d162e094fc4c9d1d8383710c409
Sascha Lehmann <sl146@hdm-stuttgart.de>
2022-04-09 10:58:42 +0200

    set up cluster_dataset methods

---
+++ modules/cluster_dataset.py
@@ -0,0 +1,11 @@
+def cluster_data(dataset, feature1, feature2, cluster_method):
+    return dataset
+
+def kmeans_cluster():
+    return 'placeholder'
+
+def affinityPropagation_cluser():
+    return 'placeholder'
+
+def set_labels():
+    return 'placeholder'
