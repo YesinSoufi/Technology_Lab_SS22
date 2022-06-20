2b48c0be85c27c9b17693cda346acede24a36b62
Sascha Lehmann <sl146@hdm-stuttgart.de>
2022-06-20 23:11:09 +0200

    update challenge 3

--- modules/Challenge_3/AudioGenerator.py
+++ modules/Challenge_3/AudioGenerator.py
@@ -1,13 +1,18 @@
 #%%
+from ast import literal_eval
 import pandas as pd
 import numpy as np
 import tensorflow as tf
+import AudioUtil
+from pathlib import Path
 
 #%%
 modelPath = 'Trained_Model/SL_CRNN_prototyp_1'
 sampleCSVDir = 'Samples_CSV/'
 sampleWAVDir = 'Samples_WAV'
 exportDir = 'New_Songs/'
+startSample = 'Samples_WAV/Runner/1Runner.wav'
+songLength = 10
 
 #%%
 #load model for prediction
@@ -30,6 +35,29 @@ exportDir = 'New_Songs/'
 #load samples from new song-list and append export es one wav file
 
 #%%
+#load model
 model = tf.keras.models.load_model('Trained_Model/SL_CRNN_prototyp_1')
 model.summary()
-# %%
+
+#load start sample
+startS, _ = AudioUtil.loadWaveform(startSample)
+len(startS)
+
+#%%
+#load samples to create song
+df_samples = pd.DataFrame(columns=['name', 'waveform'])
+
+for file in Path(sampleCSVDir).glob('*.csv'):
+    df_temp = pd.read_csv(file, )
+    df_temp['waveform'] = df_temp['waveform'].apply(literal_eval)
+    df_samples = pd.concat([df_samples, df_temp], axis=0, sort=False)
+
+del(df_temp)
+df_samples
+
+#%%
+currentSample = startS
+predSamples = []
+
+for x in range(songLength):
+    'tbd'
